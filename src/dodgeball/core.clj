5e74c485f7127a0324ada83efe75253dbfd60c46
ujihisa <ujihisa@gmail.com>
2012-03-21 08:52:15 -0700

    changes blocks around the target block/entity

--- src/dodgeball/core.clj
+++ src/dodgeball/core.clj
@@ -25,14 +25,36 @@
           direction (.multiply (.clone velocity) (double (/ 1 (.length velocity))))
           block (.getBlock (.add (.clone location) direction))
           type (.getType block)]
-      (cond
-        (= type Material/STONE) (.setType block Material/COBBLESTONE)
-        (= type Material/COBBLESTONE) (.setType block Material/AIR)))))
+      (doseq [[x z] [[-1 0] [0 -1] [1 0] [0 1] [0 0]]]
+        (let [b (.getBlock (.add (.clone (.getLocation block)) x 0 z))]
+          (cond
+            (= type Material/STONE) (.setType b Material/COBBLESTONE)
+            (= type Material/COBBLESTONE) (.setType b Material/AIR))))
+      (.remove entity))))
 
 (defn projectile-hit-event [evt]
   (let [entity (.getEntity evt)]
     (when (instance? Arrow entity)
       (arrow-hit-event evt entity))))
+
+(defn arrow-damages-entity-event [evt attacker target]
+  (let [loc (.add (.clone (.getLocation target)) 0 -1 0)]
+    (doseq [[x z] (vec (concat (map (fn [i] [i 0]) (range -2 3)) (map (fn [i] [0 i]) (range -2 3))))]
+      (let [b (.getBlock (.add (.clone loc) x 0 z))
+            type (.getType b)]
+        (cond
+          (= type Material/STONE) (.setType b Material/COBBLESTONE)
+          (= type Material/COBBLESTONE) (.setType b Material/AIR))))))
+
+(defn entity-damage-event [evt]
+  (let [target (.getEntity evt)
+        attacker (when (instance? EntityDamageByEntityEvent evt)
+                   (.getDamager evt))]
+    (when (and attacker
+               (instance? Arrow attacker)
+               (.getShooter attacker)
+               (instance? Player (.getShooter attacker)))
+      (arrow-damages-entity-event evt attacker target))))
 
 (defonce swank* nil)
 (defn on-enable [plugin]
